e71a6ad2a109ae7844db0f3de6560d1ce9c6c070
manikanta.kg <manikanta.kg@xelpmoc.in>
2022-10-13 17:28:19 +0530

    swagger added for test API

--- app.js
+++ app.js
@@ -3,8 +3,37 @@ const express = require('express');
 const { application } = require("express");
 const app = express();
 
+const swaggerJSDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
+
+const bodyparser = require('body-parser');
+const { response } = require('express');
+app.use(bodyparser.json());
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Node JS Orders Project for MYSQL',
+            version: '1.0.0'
+        },
+        servers: [
+            {
+                url: 'http://localhost:4440/'
+            }
+        ]
+    },
+    apis: ['./app.js']
+}
+
+
+const swaggerSpec = swaggerJSDoc(options)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
 const uri = "mongodb+srv://manikanta:Xelpmoc@cluster0.psvmlaj.mongodb.net/test"
  
+
+
 const client = new MongoClient(uri);
 async function run() {
   try {
@@ -20,7 +49,18 @@ async function run() {
   }
 }
 run().catch(console.dir);
-        
+   
+/**
+ * @swagger
+ * /:
+ *  get:
+ *      summary: This api is use to check if get method is working or not
+ *      description: This api is use to check if get method is working or not
+ *      responses:
+ *          200:
+ *              description: To test Get method
+ */
+
 
 app.get('/', function(req, res){
     res.send("Hello")
